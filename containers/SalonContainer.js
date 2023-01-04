0ae521b2354868e241bfb0655bc365aeb1e62587
Tomas Moltrasio <tomymoltrasio@gmail.com>
2023-01-04 16:17:41 -0300

    add swiper

--- containers/SalonContainer.js
+++ containers/SalonContainer.js
@@ -1,7 +1,13 @@
 import { Image } from "@nextui-org/react";
 import { useState } from "react";
-import { TfiArrowCircleRight, TfiArrowCircleLeft } from "react-icons/tfi";
 import { BsWhatsapp } from "react-icons/bs";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import { EffectCards } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cards";
 
 export default function SalonContainer() {
   const images = [
@@ -42,55 +48,29 @@ export default function SalonContainer() {
 
   return (
     <div className="w-full h-max flex flex-col items-center justify-start">
-      <div className="w-full md:w-3/6 lg:w-3/6 xl:w-3/6 2xl:w-3/6 h-4/5 mt-4 flex flex-col items-center relative justify-center">
-        <Image
-          src={images[index]}
-          alt="Salon"
-          height="540px"
-          width="100%"
-          objectFit="cover"
-          className="rounded-2xl shadow-2xl"
-        />
-        <button aria-label="Anterior imagen" onClick={handlePrev}>
-          <TfiArrowCircleLeft
-            size={40}
-            className="
-            absolute
-            top-1/2
-            left-0
-            transform
-            text-3xl
-            text-white
-            bg-black
-            rounded-full
-            p-1
-            hover:bg-gray-800
-            transition
-            duration-300
-            ease-in-out 
-          "
-          />
-        </button>
-        <button aria-label="Siguiente imagen" onClick={handleNext}>
-          <TfiArrowCircleRight
-            size={40}
-            className="
-            absolute
-            top-1/2
-            right-0
-            transform
-            text-3xl
-            text-white
-            bg-black
-            rounded-full
-            hover:bg-gray-800
-            p-1
-            transition
-            duration-300
-            ease-in-out
-            "
-          />
-        </button>
+      <div className="w-full md:w-3/6 lg:w-3/6 xl:w-3/6 2xl:w-3/6 h-max mt-4 flex flex-col items-center relative justify-center">
+        <Swiper
+          effect={"cards"}
+          grabCursor={true}
+          modules={[EffectCards]}
+          spaceBetween={50}
+          slidesPerView={1}
+          rewind={true}
+          className="w-full h-max"
+        >
+          {images.map((image, index) => (
+            <SwiperSlide key={index}>
+              <Image
+                src={image}
+                alt="Salon"
+                height="540px"
+                width="100%"
+                objectFit="cover"
+                className="rounded-xl shadow-2xl"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
       <div className="w-full md:w-3/6 lg:w-3/6 xl:w-3/6 2xl:w-3/6 h-max my-4 px-2 flex flex-row items-center justify-between">
         <div className="group flex flex-row w-full items-center justify-center gap-2 p-1 rounded-xl border-2 px-4 border-black hover:scale-105 hover:border-lime-500 hover:text-slate-800 transition duration-300 ease-in-out hover:font-semibold">
