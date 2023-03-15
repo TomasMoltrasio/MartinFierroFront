1016c82254de11f22148085765096d09e6417d3f
Tomas Moltrasio <tomymoltrasio@gmail.com>
2023-03-15 19:40:58 -0300

    change map

--- containers/MapContainer.js
+++ containers/MapContainer.js
@@ -2,15 +2,15 @@ export default function MapContainer() {
   return (
     <div>
       <iframe
-        width={550}
-        height={400}
-        frameBorder={0}
-        src="https://widgets.scribblemaps.com/sm/?d&z&l&gc&af&mc&dfe&lat=-35.57823354&lng=-58.015565323&vz=12&type=road&ti&s&width=550&height=400&id=MartinFierro"
+        width="550"
+        height="400"
+        frameborder={0}
+        src="https://widgets.scribblemaps.com/sm/?d&z&l&gc&af&mc&lat=-35.54330448&lng=-58.125986488&vz=11&type=road&ti&s&width=550&height=400&id=MartinFierro"
         style={{ border: 0, maxWidth: "100%" }}
-        allowFullScreen
+        allowfullscreen
         allow="geolocation"
         loading="lazy"
-      />
+      ></iframe>
     </div>
   );
 }
