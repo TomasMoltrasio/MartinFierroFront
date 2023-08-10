f8021e6b011c34839e17b3630f9f1dcdd8b9f869
Tomas Moltrasio <tomymoltrasio@gmail.com>
2023-08-10 11:47:28 -0300

    change price

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -67,7 +67,7 @@ export default function CreateProduct({
       if (response) {
         await validated("/menu").finally(() => {
           Swal.fire({
-            title: "Producto creado con exito",
+            title: "Producto creado con exito.",
             icon: "success",
             confirmButtonText: "Ok",
           }).then(() => {
