40a2287a78851046f254846d7b3ae8966936ad0e
Tomas Moltrasio <tomymoltrasio@gmail.com>
2023-10-14 14:02:16 -0300

    change map

--- containers/MapContainer.js
+++ containers/MapContainer.js
@@ -5,7 +5,7 @@ export default function MapContainer() {
         width="550"
         height="400"
         frameborder={0}
-        src="https://widgets.scribblemaps.com/sm/?d&z&l&gc&af&mc&lat=-35.54330448&lng=-58.125986488&vz=11&type=road&ti&s&width=550&height=400&id=MartinFierro"
+        src="https://widgets.scribblemaps.com/sm/?d&dv&cv&z&l&gc&af&mc&lat=-35.59873698&lng=-58.08530281&vz=12&type=road&ti&s&width=550&height=400&id=MartinFierro"
         style={{ border: 0, maxWidth: "100%" }}
         allowfullscreen
         allow="geolocation"
